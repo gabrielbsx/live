ce2f0cb5d9b4fa4071d6b21096741bda46baa860
Gabriel Barbosa <gabriel.barbosa@gentell.com>
2024-11-24 03:00:34 -0300

    chore: add aeria sdk and config for react vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2022"
+  },
+  esbuild: {
+    target: "es2022"
+  },
+  optimizeDeps:{
+    esbuildOptions: {
+      target: "es2022",
+    }
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
